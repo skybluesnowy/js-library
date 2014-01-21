13bb564f99b98119cff94ff825915eaaf4bd8723
Frazer Harte <frazer.harte@razorfish.com>
2014-01-21 15:18:47 +0000

    Some tinkering

--- scrolling/static/js/global.js
+++ scrolling/static/js/global.js
@@ -1,31 +1,35 @@
 // Js //
 $(document).ready(function(){
 
-$(window).scroll(function () {
-    if ($(window).scrollTop() >= 400) {
-        $('#two').fadeIn('slow');
-    }
-    if ($(window).scrollTop() >= 580) {
-        $('#two p.delayed').fadeIn('slow');
-    }
-    if ($(window).scrollTop() <= 550) {
-        $('#two p.delayed').fadeOut('slow');
-    }
-    if ($(window).scrollTop() >= 1200) {
-        $('#three').fadeIn('slow');
-    }
-    if ($(window).scrollTop() >= 1800) {
-        $('#four').fadeIn('slow');
-    }
-  /*  if ($(window).scrollTop() <= 299) {
-        $('#two').fadeOut('slow');
-    }
-    if ($(window).scrollTop() <= 599) {
-        $('#three').fadeOut('slow');
-    }
-    if ($(window).scrollTop() <= 899) {
-        $('#four').fadeOut('slow');
-    }*/
-   
-});
+	$(window).scroll(function () {
+		if ($(window).scrollTop() >= 300) {
+			$('#two').fadeIn('slow');
+		}
+		if ($(window).scrollTop() >= 580) {
+			$('#two p.delayed').fadeIn('slow');
+		}
+		if ($(window).scrollTop() <= 550) {
+			$('#two p.delayed').fadeOut('slow');
+		}
+		if ($(window).scrollTop() >= 1000) {
+			$('#three').fadeIn('slow');
+		}
+		if ($(window).scrollTop() >= 1500) {
+			$('#four').fadeIn('slow');
+		}
+
+		  /*  if ($(window).scrollTop() <= 299) {
+		        $('#two').fadeOut('slow');
+		    }
+		    if ($(window).scrollTop() <= 599) {
+		        $('#three').fadeOut('slow');
+		    }
+		    if ($(window).scrollTop() <= 899) {
+		        $('#four').fadeOut('slow');
+		    }*/
+		});
+	$(window).resize(function(){
+		$('#sidebar').height($(window).height());
+	});	
+	$('#sidebar').height($(window).height());
 });
